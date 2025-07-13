281e11a3fc267909a3d63c9cbea6a0eb8a868585
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2025-07-13 15:14:16 -0300

    Otras Validaciones por Defecto

--- ManejoPresupuesto/Models/TipoCuenta.cs
+++ ManejoPresupuesto/Models/TipoCuenta.cs
@@ -11,5 +11,16 @@ namespace ManejoPresupuesto.Models
         public string Nombre { get; set; }
         public int UsuarioId { get; set; }
         public int Orden { get; set; }
+
+        /** Pruebas de otras validaciones por defecto **/
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [EmailAddress(ErrorMessage = "El campo {0} debe ser un correo electrónico válido")]
+        public string Email { get; set; }
+        [Range(minimum: 18, maximum: 100, ErrorMessage = "La {0} debe estar entre {1} y {2}")]
+        public int Edad { get; set; }
+        [Url(ErrorMessage = "El campo {0} debe ser una URL válida")]
+        public string URL { get; set; }
+        [CreditCard(ErrorMessage = "El campo {0} debe ser un número de tarjeta de crédito válido")]
+        public string TarjetaDeCredito { get; set; }
     }
 }
