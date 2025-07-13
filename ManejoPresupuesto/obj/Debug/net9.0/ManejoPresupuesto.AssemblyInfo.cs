de52635e68b74b76b0e1554c6b310e9279d7b3c8
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2025-07-13 14:37:40 -0300

    Visualizando los Errores de Validación

--- ManejoPresupuesto/obj/Debug/net9.0/ManejoPresupuesto.AssemblyInfo.cs
+++ ManejoPresupuesto/obj/Debug/net9.0/ManejoPresupuesto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ManejoPresupuesto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55685f23c0954a4ddc80ab02301dcc0f1c17f2de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3a42deceba18b10ed104d319076ae29d89306b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ManejoPresupuesto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ManejoPresupuesto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
