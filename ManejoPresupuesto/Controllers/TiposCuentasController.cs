aa30b2387b4092acda28313157e02284a9d3d38d
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2025-10-05 14:01:28 -0300

    Haciendo un post con el FETCH API

--- ManejoPresupuesto/Controllers/TiposCuentasController.cs
+++ ManejoPresupuesto/Controllers/TiposCuentasController.cs
@@ -129,5 +129,11 @@ namespace ManejoPresupuesto.Controllers
 
             return Json(true);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Ordenar([FromBody] int[] ids)
+        {
+            return Ok();
+        }
     }
 }
