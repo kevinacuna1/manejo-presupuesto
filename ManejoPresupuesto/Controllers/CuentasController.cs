77ef4ee696214ac83d4543c42f151aa2c41fc4c2
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2025-10-08 23:58:19 -0300

    Formulario de Cuentas

---
+++ ManejoPresupuesto/Controllers/CuentasController.cs
@@ -0,0 +1,32 @@
+﻿using ManejoPresupuesto.Servicios;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ManejoPresupuesto.Controllers
+{
+    public class CuentasController : Controller
+    {
+        public IRepositorioTiposCuentas RepositorioTiposCuentas { get; }
+        public IServicioUsuarios ServicioUsuarios { get; }
+
+        public CuentasController(IRepositorioTiposCuentas repositorioTiposCuentas, IServicioUsuarios servicioUsuarios)
+        {
+            RepositorioTiposCuentas = repositorioTiposCuentas;
+            ServicioUsuarios = servicioUsuarios;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Crear()
+        {
+            var usuarioId = ServicioUsuarios.ObtenerUsuarioId();
+
+            var tiposCuentas = await RepositorioTiposCuentas.Obtener(usuarioId);
+
+            var modelo = new Models.CuentaCreacionViewModel();
+
+            modelo.TiposCuentas = tiposCuentas.Select(x => new SelectListItem(x.Nombre, x.Id.ToString()));
+
+            return View(modelo);
+        }
+    }
+}
---
+++ ManejoPresupuesto/Models/Cuenta.cs
@@ -0,0 +1,19 @@
+﻿using ManejoPresupuesto.Validaciones;
+using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Models
+{
+    public class Cuenta
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [StringLength(50, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres")]
+        [PrimeraLetraMayuscula]
+        public string Nombre { get; set; }
+        [Display(Name = "Tipo de cuenta")]
+        public int TipoCuentaId { get; set; }
+        public decimal Balance { get; set; }
+        [StringLength(200, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres")]
+        public string Descripcion { get; set; }
+    }
+}
